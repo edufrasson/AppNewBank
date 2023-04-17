ad4e56c0785e7d7f120c858b570af41298cc7091
edufrasson <edudomine2016@gmail.com>
2023-04-17 08:48:49 -0300

    Adicionar arquivos de projeto.

---
+++ AppNewBank/AppNewBank/Model/ChavePix.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppNewBank.Model
+{
+    public class ChavePix
+    {
+        public int Id { get; set; }
+        public string Tipo { get; set; }
+        public string Chave { get; set; }
+        public int Id_Conta { get; set; }
+    }
+}
