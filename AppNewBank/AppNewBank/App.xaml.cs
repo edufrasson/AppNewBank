6a1bc5ff912285211047e479e03b8b73a08f6660
edufrasson <edudomine2016@gmail.com>
2023-05-04 13:51:47 -0300

    feat: arquivos de login / novo correntista

--- AppNewBank/AppNewBank/App.xaml.cs
+++ AppNewBank/AppNewBank/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppNewBank.View.Correntista;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace AppNewBank
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new Login());
         }
 
         protected override void OnStart()
---
+++ AppNewBank/AppNewBank/View/Correntista/NovoCorrentista.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppNewBank.View.Correntista
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NovoCorrentista : ContentPage
+	{
+		public NovoCorrentista ()
+		{
+			InitializeComponent ();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+	}
+}
