b3fcd211484e230ca8c7f57113f5ed7026c02036
edufrasson <edudomine2016@gmail.com>
2023-11-22 09:37:24 -0300

    feat: gerando qrcode

--- AppNewBank/AppNewBank/View/Funcoes/ReceberPix.xaml.cs
+++ AppNewBank/AppNewBank/View/Funcoes/ReceberPix.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using QRCoder;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +19,17 @@ namespace AppNewBank.View.Funcoes
             InitializeComponent();
 
             NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string teste = "Chave da Transferência: tiago@tiago.blog.br ";
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(teste, QRCodeGenerator.ECCLevel.Q);
+            PngByteQRCode qRCode = new PngByteQRCode(qrCodeData);
+            byte[] qrCodeBytes = qRCode.GetGraphic(20);
+            img_qrcode.Source = ImageSource.FromStream(() => new MemoryStream(qrCodeBytes));
         }
     }
 }
